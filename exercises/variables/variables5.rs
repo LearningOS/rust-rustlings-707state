3c7165bd3fabf72e92ec534cf18b34e60f0a14af
707state <2713427119@qq.cm>
2023-10-09 15:12:32 +0800

    update

--- exercises/variables/variables5.rs
+++ exercises/variables/variables5.rs
@@ -1,11 +1,13 @@
 // variables5.rs
 // Execute `rustlings hint variables5` or use the `hint` watch subcommand for a hint.
 
-// I AM NOT DONE
-
 fn main() {
-    let number = "T-H-R-E-E"; // don't change this line
+    let number:&str = "T-H-R-E-E"; // don't change this line
     println!("Spell a Number : {}", number);
-    number = 3; // don't rename this variable
+    // number= 3; // don't rename this variable
+    let number =match number{
+        "T-H-R-E-E"=>3,
+        _=>panic!("invalid number"),
+    };
     println!("Number plus two is : {}", number + 2);
 }
