73573390596b077e98e21d02596fd3af78f2e5fc
707state <2713427119@qq.cm>
2023-10-10 22:45:25 +0800

    update

--- exercises/standard_library_types/cow1.rs
+++ exercises/standard_library_types/cow1.rs
@@ -5,11 +5,10 @@
 // It can enclose and provide immutable access to borrowed data, and clone the data lazily when mutation or ownership is required.
 // The type is designed to work with general borrowed data via the Borrow trait.
 
-// I AM NOT DONE
 
 use std::borrow::Cow;
 
-fn abs_all<'a, 'b>(input: &'a mut Cow<'b, [i32]>) -> &'a mut Cow<'b, [i32]> {
+fn abs_all<'a,'b>(input: &'a mut Cow<'b, [i32]>) ->&'a mut Cow<'b, [i32]> {
     for i in 0..input.len() {
         let v = input[i];
         if v < 0 {
@@ -41,8 +40,8 @@ fn main() {
     let slice = vec![-1, 0, 1];
     let mut input = Cow::from(slice);
     match abs_all(&mut input) {
-        // TODO
-        Cow::Borrowed(_) => println!("I own this slice!"),
+        
+        Cow::Owned(_)=>println!("I own this slice!"),
         _ => panic!("expected borrowed value"),
     }
 }
